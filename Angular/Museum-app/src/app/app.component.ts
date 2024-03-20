7e8b4b321b325f679790b8f4c37eafd17d9499eb
Cipher <cipher@rohannk.com>
2024-03-20 14:58:08 +0530

    Imported from miniprojects

---
+++ Angular/Museum-app/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <app-header></app-header>
+    <router-outlet />
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'Museum-app';
+}
